3f9e5fef8045a2208d68eda25209f807abd15cd1
bvreeman <bvreeman@gmail.com>
2019-04-25 20:43:59 -0500

    working through a php tutorial

---
+++ sandbox/helloworld.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Hello World!</title>
+    </head>
+    <body>
+    <?php  
+    // single-line comments are like this
+    # or like this (less common)
+    /*  double line comments
+        that keep going
+        and typing
+    */
+    ?>
+    <?php echo "Hello World! This is me!"; ?><br />
+    <?php echo "Hello" . "World! This is me!"; ?><br />
+    <?php echo 2 + 3; ?>
+    </body>
+</html>
---
+++ sandbox/variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Variables</title>
+    </head>
+    <body>
+        <?php
+
+        $var1 = 10;
+        echo $var1;
+
+        echo "<br />";
+        
+        $var1 = 100;
+        echo $var1;
+
+        ?>
+    </body>
+</html>
