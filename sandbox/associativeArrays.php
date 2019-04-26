e60d524f97be14d5a73e79d76e2f82dfc8209855
bvreeman <bvreeman@gmail.com>
2019-04-25 21:09:30 -0500

    associative array work

--- sandbox/associativeArrays.php
+++ sandbox/associativeArrays.php
@@ -6,8 +6,19 @@
     <body>
         <?php
 
-        
-
+        $assoc = array("first_name" => "Brandon",
+        "last_name" => "Vreeman");
         ?>
+        <?php echo $assoc["first_name"]; ?><br />
+        <?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?><br />
+
+        <?php $assoc["first_name"] = "Larry"; ?>
+        <?php echo $assoc["first_name"] . " " . $assoc["last_name"]; ?><br />
+    
+        <?php // echo $assoc[0]; ?><br />
+
+        <?php $numbers = array(4,8,16,23,42); ?>
+        <?php $numbers = array(0 => 4, 1 => 8, 2 => 15, 3 => 16, 4 => 23, 5 => 42); ?>
+        <?php echo $numbers[0]; ?>
     </body>
 </html>
