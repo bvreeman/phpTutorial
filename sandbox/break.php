7f3fc628e09df976db62942c5ea96921f5b053a4
bvreeman <bvreeman@gmail.com>
2019-05-03 17:08:56 -0500

    went through the break tutorial

---
+++ sandbox/break.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Break</title>
+    </head>
+    <body>
+        <?php
+            for ($count=0; $count <= 10; $count++) {
+                if ($count == 5) {
+                    break;
+                }
+                echo $count . ", ";
+            }
+        ?>
+        <br />
+        <?php
+            for ($i=0; $i<=5; $i++) {
+                if ($i % 2 == 0) { continue(1); }
+                for ($k=0; $k<=5; $k++) {
+                    if ($k ==3) { break; };
+                    echo $i . "-" .  $k . "<br />";
+                }
+            }
+        ?>
+                <br />
+        <?php
+            for ($i=0; $i<=5; $i++) {
+                if ($i % 2 == 0) { continue(1); }
+                for ($k=0; $k<=5; $k++) {
+                    if ($k ==3) { break(2); };
+                    echo $i . "-" .  $k . "<br />";
+                }
+            }
+        ?>
+    </body>
+</html>
